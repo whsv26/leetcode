5b38359c7227faa204ba11b73affda7168fa0b29
whsv26 <whsv26@gmail.com>
2022-11-04 23:41:08 +0300

    Symmetric tree iterative solution.

--- src/main/scala/tree/SymmetricTree.scala
+++ src/main/scala/tree/SymmetricTree.scala
@@ -1,14 +1,17 @@
 package org.whsv26.leetcode
 package tree
 
+import scala.collection.mutable
+import scala.util.control.Breaks.{break, breakable}
+
 object SymmetricTree {
 
   def main(args: Array[String]): Unit = {
-    assert(isSymmetric(mkTree(1,2,2,3,4,4,3)))
-    assert(!isSymmetric(mkTree(1,2,2,null,3,null,3)))
+    assert(isSymmetricIterative(mkTree(1,2,2,3,4,4,3)))
+    assert(!isSymmetricRecursive(mkTree(1,2,2,null,3,null,3)))
   }
 
-  def isSymmetric(root: TreeNode): Boolean = {
+  def isSymmetricRecursive(root: TreeNode): Boolean = {
     def iter(lhs: TreeNode, rhs: TreeNode): Boolean =
       if (lhs == null && rhs == null) {
         true
@@ -19,5 +22,31 @@ object SymmetricTree {
       }
 
     iter(root.left, root.right)
+  }
+
+  def isSymmetricIterative(root: TreeNode): Boolean = {
+    val stack = mutable.Stack((root.left, root.right))
+    var symmetric = true
+
+    breakable {
+      while (stack.nonEmpty && symmetric) {
+        val (lhs, rhs) = stack.pop()
+
+        if ((lhs == null) ^ (rhs == null)) {
+          symmetric = false
+          break
+        } else if (lhs != null) {
+          if (lhs.value != rhs.value) {
+            symmetric = false
+            break
+          }
+
+          stack.push((lhs.right, rhs.left))
+          stack.push((lhs.left, rhs.right))
+        }
+      }
+    }
+
+    symmetric
   }
 }
