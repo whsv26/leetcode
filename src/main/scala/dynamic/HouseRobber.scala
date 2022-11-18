4c56c887a20aa08c7eda4f1e0234e74d65f8c436
whsv26 <whsv26@gmail.com>
2022-11-19 01:14:03 +0300

    House robber solution.

--- src/main/scala/dynamic/HouseRobber.scala
+++ src/main/scala/dynamic/HouseRobber.scala
@@ -9,36 +9,18 @@ object HouseRobber {
     assert(4 == rob(Array(2,1,1,2)))
   }
 
-//  def rob(nums: Array[Int]): Int = {
-//
-//    val cache = Array.fill(3)(0)
-//    var max = 0
-//    cache(2) = nums(0)
-//
-//    for (i <- 1 until nums.length) {
-//      val tmp = cache(0)
-//      cache(0) = cache(1)
-//      cache(1) = cache(2)
-//      cache(2) = tmp + nums(i)
-//      max = Math.max(max, cache(2))
-//    }
-//
-//    cache.max
-//  }
-
   def rob(nums: Array[Int]): Int = {
 
-    val cache1 = Array.fill(nums.length)(0)
-    val cache2 = Array.fill(nums.length)(0)
-    cache1(0) = nums(0)
-    cache1(nums.length - 1) = nums(nums.length - 1)
+    val cache = Array.fill(nums.length)(0)
+    cache(0) = nums(0)
 
-    for (i <- 0 until nums.length) {
-      val j = nums.length - 1 - i
-      cache1(i) = if (cache1.isDefinedAt(i - 2)) cache1(i - 2) else 0 + nums(i)
-      cache2(j) = if (cache2.isDefinedAt(j + 2)) cache2(j + 2) else 0 + nums(j)
+    for (i <- 1 until nums.length) {
+      val b2 = if (cache.isDefinedAt(i - 2)) cache(i - 2) else 0
+      val b3 = if (cache.isDefinedAt(i - 3)) cache(i - 3) else 0
+
+      cache(i) = Math.max(b2, b3) + nums(i)
     }
 
-    cache1.zip(cache2).map(x => x._1 + x._2).max
+    cache.max
   }
 }
