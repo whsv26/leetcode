4fc7800e956d9093ee68dbf2370788795d15fb05
whsv26 <whsv26@gmail.com>
2022-11-19 15:08:10 +0300

    FizzBuzz solution.

---
+++ src/main/scala/math/FizzBuzz.scala
@@ -0,0 +1,32 @@
+package org.whsv26.leetcode
+package math
+
+import scala.collection.mutable.ListBuffer
+
+object FizzBuzz {
+
+  def main(args: Array[String]): Unit = {
+    println(fizzBuzz(5))
+  }
+
+  def fizzBuzz(n: Int): List[String] = {
+    val res = ListBuffer.empty[String]
+
+    for (i <- 1 to n) {
+      val elem = if (i % 3 == 0 && i % 5 == 0) {
+        "FizzBuzz"
+      } else if (i % 3 == 0) {
+        "Fizz"
+      } else if (i % 5 == 0) {
+        "Buzz"
+      } else {
+        i.toString
+      }
+
+      res.append(elem)
+    }
+
+    res.result()
+  }
+
+}
