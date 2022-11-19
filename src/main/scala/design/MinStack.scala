ae4baad6852960cd349ff638ee02e0966f6d9b3d
whsv26 <whsv26@gmail.com>
2022-11-19 14:53:26 +0300

    Min stack solution.

---
+++ src/main/scala/design/MinStack.scala
@@ -0,0 +1,33 @@
+package org.whsv26.leetcode
+package design
+
+import java.lang.Math.min
+
+object MinStack {
+
+  def main(args: Array[String]): Unit = ???
+
+  class Solution() {
+    private var list: List[(Int, Int)] = Nil
+
+    def push(`val`: Int) {
+      list = list match {
+        case Nil => (`val`, `val`) :: list
+        case (_, bot) :: _ => (`val`, min(`val`, bot)) :: list
+      }
+    }
+
+    def pop() {
+      list = list.tail
+    }
+
+    def top(): Int = {
+      list.head._1
+    }
+
+    def getMin(): Int = {
+      list.head._2
+    }
+
+  }
+}
