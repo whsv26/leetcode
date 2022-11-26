2e10bde4410561951aeeff131dd01b6290d97f9a
whsv26 <whsv26@gmail.com>
2022-11-26 03:46:23 +0300

    Reverse bits solution.

--- src/main/scala/others/ReverseBits.scala
+++ src/main/scala/others/ReverseBits.scala
@@ -1,34 +1,50 @@
 package org.whsv26.leetcode
 package others
 
-import java.lang.Math.pow
-
 object ReverseBits {
 
   def main(args: Array[String]): Unit = {
-    println("NUM = " + Integer.parseUnsignedInt("1010101010101010", 2).toBinaryString)
-    println("1010101010101010 -> " + reverseBits(Integer.parseUnsignedInt("1010101010101010", 2)).toBinaryString)
+    println(reverseBits(Integer.parseUnsignedInt("00000010100101000001111010011100", 2)).toBinaryString)
+  }
+
+  def reverseBitsInlined(x: Int): Int = {
+    var res = x
+
+    for (i <- 0 until 16) {
+      res = set(set(res, i, (res & (1 << 31 - i)) >> 31 - i), 31 - i, (res & (1 << i)) >> i)
+    }
+
+    res
   }
 
   def reverseBits(x: Int): Int = {
     var res = x
 
     for (i <- 0 until 16) {
-      val j = 30 - i
-      val unsetted = res & ~(1 << i) & ~(1 << j)
-      val swappedA = (x & (1 << i)) << j
-      val swappedB = (x & (1 << j)) >> j
-      val swapped = swappedA | swappedB
-
-      val swappedABinaryString = swappedA.toBinaryString
-
-      res = unsetted | swapped
-      println(s"swapped = swappedA(${swappedABinaryString}) | swappedB(${swappedB.toBinaryString})")
-      println(s"unsetted(${unsetted.toBinaryString}) | swapped(${swapped.toBinaryString}) = ${res.toBinaryString}")
-      println
+      res = swap(res, i, 31 - i)
     }
 
     res
   }
 
+  @inline def swap(number: Int, i: Int, j: Int): Int =
+    set(set(number, i, get(number, j)), j, get(number, i))
+
+  @inline def get(number: Int, pos: Int): Int =
+    (number & (1 << pos)) >> pos
+
+  @inline def set(number: Int, pos: Int, v: Int): Int =
+    number ^ (-v ^ number) & (1 << pos)
+
+  /**
+   * shift range is 0-31
+   * ~(1 << i) trick is not working if i == 31
+   */
+  def swapUnsafe(i: Int, j: Int)(n: Int): Int = {
+    val unsetted = n & ~(1 << i) & ~(1 << j)
+    val swapLeft = (n & (1 << i)) << (j - i)
+    val swapRight = (n & (1 << j)) >> (j - i)
+    val swapped = swapLeft | swapRight
+    unsetted | swapped
+  }
 }
