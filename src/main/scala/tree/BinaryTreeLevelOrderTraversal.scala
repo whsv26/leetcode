c5830a2cb63bada0eb2f2193a13d98f877a0cd56
whsv26 <whsv26@gmail.com>
2023-01-30 18:59:52 +0300

    Binary tree level order traversal immutable impl.

--- src/main/scala/tree/BinaryTreeLevelOrderTraversal.scala
+++ src/main/scala/tree/BinaryTreeLevelOrderTraversal.scala
@@ -1,17 +1,37 @@
 package org.whsv26.leetcode
 package tree
 
+import scala.collection.immutable.Queue
 import scala.collection.mutable
 
 object BinaryTreeLevelOrderTraversal {
 
   def main(args: Array[String]): Unit = {
-    println(levelOrder(mkTree(3,9,20,null,null,15,7))) // [[3],[9,20],[15,7]]
-    println(levelOrder(mkTree(1,2,null,3,null,4,null,5))) // [[1],[2],[3],[4],[5]]
-    println(levelOrder(mkTree(1,2,3,4,5))) // [[1],[2,3],[4,5]]
+    println(levelOrderImmutable(mkTree(3,9,20,null,null,15,7))) // [[3],[9,20],[15,7]]
+    println(levelOrderImmutable(mkTree(1,2,null,3,null,4,null,5))) // [[1],[2],[3],[4],[5]]
+    println(levelOrderImmutable(mkTree(1,2,3,4,5))) // [[1],[2,3],[4,5]]
   }
 
-  def levelOrder(root: TreeNode): List[List[Int]] = {
+  def levelOrderImmutable(root: TreeNode): List[List[Int]] = {
+    if (root == null)
+      return Nil
+
+    List.unfold(Queue(root)) {
+      case queue if queue.isEmpty => None
+      case queue =>
+        val currentLevel = queue.toList.map(_.value)
+        val nextLevel = queue.foldLeft(Queue.empty[TreeNode]) {
+          case (acc, cur) =>
+            List(Option(cur.left), Option(cur.right))
+              .flatten
+              .foldLeft(acc)(_ enqueue _)
+        }
+
+        Some(currentLevel, nextLevel)
+    }
+  }
+
+  def levelOrderMutable(root: TreeNode): List[List[Int]] = {
     if (root == null)
       return Nil
 
