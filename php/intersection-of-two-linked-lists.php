85daa1a2b3761ffe1b5fb3e4211b226d9d7f080d
whsv26 <whsv26@gmail.com>
2024-02-17 04:35:42 +0300

    Split project into php and scala modules.

---
+++ php/intersection-of-two-linked-lists.php
@@ -0,0 +1,26 @@
+<?php
+
+class ListNode
+{
+    public mixed $val = 0;
+    public ?ListNode $next = null;
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
+class Solution
+{
+    /**
+     * @param ListNode $headA
+     * @param ListNode $headB
+     * @return ListNode
+     */
+    function getIntersectionNode(ListNode $headA, ListNode $headB): ListNode
+    {
+
+    }
+}
+
--- scala/build.sbt
+++ scala/build.sbt
@@ -4,7 +4,7 @@ ThisBuild / scalaVersion := "2.13.10"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "leetcode",
+    name := "scala",
     idePackagePrefix := Some("org.whsv26.leetcode")
   )
 
