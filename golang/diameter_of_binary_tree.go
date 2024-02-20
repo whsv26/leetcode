bb72dedd617b2aa2c7f20df870a22a86e7247cb8
whsv26 <whsv26@gmail.com>
2024-02-20 17:30:57 +0300

    Diameter of Binary Tree

--- golang/diameter_of_binary_tree.go
+++ golang/diameter_of_binary_tree.go
@@ -1,86 +1,26 @@
 package golang
 
-type Point struct {
-	Node   *TreeNode
-	Length int
-}
+import "math"
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	var loop func(*TreeNode) []*Point
+	var loop func(*TreeNode) (float64, float64)
 
-	loop = func(node *TreeNode) []*Point {
+	loop = func(node *TreeNode) (float64, float64) {
 		if node == nil {
-			return []*Point{{nil, 0}}
+			return 0, 0
 		}
 
-		lss := loop(node.Left)
-		for _, ls := range lss {
-			if ls.Length == 0 || ls.Node == node.Left {
-				ls.Node = node
-				ls.Length++
-			}
-		}
+		lx, ly := loop(node.Left)
+		rx, ry := loop(node.Right)
 
-		rss := loop(node.Right)
-		for _, rs := range rss {
-			if rs.Length == 0 || rs.Node == node.Right {
-				rs.Node = node
-				rs.Length++
-			}
-		}
+		y := math.Max(ly, ry) + 1
+		x := math.Max(lx, rx)
+		x = math.Max(x, ly+ry+1)
 
-		var merged []*Point
-
-		for _, ls := range lss {
-			for _, rs := range rss {
-				if ls.Node == rs.Node {
-					merged = append(merged, &Point{nil, ls.Length + rs.Length - 1})
-				}
-			}
-		}
-
-		all := append(lss, rss...)
-		all = append(all, merged...)
-		return all
+		return x, y
 	}
 
-	diameter := 0
+	x, y := loop(root)
 
-	res := loop(root)
-
-	for i := range res {
-		if res[i].Length > diameter {
-			diameter = res[i].Length
-		}
-	}
-
-	return diameter - 1
+	return int(math.Max(x, y)) - 1
 }
-
-//     4
-//    /
-//   2
-//  / \
-// 1   3
-
-//[[1]] [[3]]
-//[[1, 2], [2, 3], [1, 2, 3]]
-
-//                   1
-//                  / \
-//                 2   11
-//                / \
-//               3   7
-//              /   /  \
-//             4   8    10
-//            / \   \
-//           5   6   9
-
-// [[5]] [[6]]                                 [[9]]
-// [[5, 4], [6, 4], [5, 4, 6]]                 [[9, 8]] [[10]]
-// [[5, 4, 3], [6, 4, 3], [5, 4, 6]]           [[9, 8, 7], [10, 7], [9, 8, 7, 10]]
-// [[5, 4, 3, 2], [6, 4, 3, 2], [5, 4, 6]]     [[9, 8, 7, 2], [10, 7, 2], [9, 8, 7, 10]]
-
-// Grow left by n if n.left == last node
-// Grow right by n if n.right == last node
-// Merge left and right with n in the middle
