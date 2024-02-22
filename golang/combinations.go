3650e9825e220d4d54d35239b780e4b98b3c976c
whsv26 <whsv26@gmail.com>
2024-02-22 17:19:19 +0300

    Combinations optimization

--- golang/combinations.go
+++ golang/combinations.go
@@ -3,26 +3,29 @@ package golang
 import "slices"
 
 func combinations(nums []int, k int) [][]int {
-	var loop func([]int, int)
+	var loop func(int)
 
 	var results [][]int
+	var stack []int
 
-	loop = func(combination []int, start int) {
-		if len(combination) == k {
-			results = append(results, combination)
+	loop = func(start int) {
+		if len(stack) == k {
+			results = append(results, slices.Clone(stack))
 			return
 		}
 
-		requested := k - len(combination)
+		requested := k - len(stack)
 		available := func(i int) int {
 			return len(nums) - i
 		}
+
 		for i := start; i < len(nums) && requested <= available(i); i++ {
-			nextCombination := append(slices.Clone(combination), nums[i])
-			loop(nextCombination, i+1)
+			stack = append(stack, nums[i])
+			loop(i + 1)
+			stack = stack[:len(stack)-1]
 		}
 	}
 
-	loop(nil, 0)
+	loop(0)
 	return results
 }
