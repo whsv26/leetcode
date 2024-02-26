97ba12cd3433f43ac893883468913c099056ad45
whsv26 <whsv26@gmail.com>
2024-02-26 13:14:16 +0300

    Combinations

--- golang/combinations_test.go
+++ golang/combinations_test.go
@@ -49,3 +49,28 @@ func Test_combinations(t *testing.T) {
 		})
 	}
 }
+
+func Test_combine(t *testing.T) {
+	tests := []struct {
+		N        int
+		K        int
+		Expected [][]int
+	}{
+		{4, 2, [][]int{{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}}},
+		{1, 1, [][]int{{1}}},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Example %v", i+1), func(t *testing.T) {
+			results := [][][]int{
+				combine(test.N, test.K),
+			}
+
+			for _, actual := range results {
+				if !reflect.DeepEqual(actual, test.Expected) {
+					t.Errorf("Expected %v, got %v", test.Expected, actual)
+				}
+			}
+		})
+	}
+}
