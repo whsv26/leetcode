ef47ba03b1b0a37672d71b8079654ac5101b093f
whsv26 <whsv26@gmail.com>
2024-02-26 13:48:20 +0300

    Three sum

---
+++ golang/3sum.go
@@ -0,0 +1,32 @@
+package golang
+
+import (
+	"slices"
+)
+
+func threeSum(nums []int) [][]int {
+	var res [][]int
+	slices.Sort(nums)
+
+	for i := range nums {
+		if i != 0 && nums[i] == nums[i-1] {
+			continue // skip duplicates
+		}
+		j, k := i+1, len(nums)-1
+		for j < k {
+			if sum := nums[i] + nums[j] + nums[k]; sum < 0 {
+				j++
+			} else if sum > 0 {
+				k--
+			} else {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				j++
+				for j < k && nums[j] == nums[j-1] {
+					j++ // skip duplicates
+				}
+			}
+		}
+	}
+
+	return res
+}
