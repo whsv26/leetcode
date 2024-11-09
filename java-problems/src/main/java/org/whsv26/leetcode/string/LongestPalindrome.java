1bf6d6d64dd44ccda73a3ef063d1e929dacaccf8
whsv26 <whsv26@gmail.com>
2024-11-10 02:32:23 +0300

    Longest Palindrome java solution

---
+++ java-problems/src/main/java/org/whsv26/leetcode/string/LongestPalindrome.java
@@ -0,0 +1,26 @@
+package org.whsv26.leetcode.string;
+
+import java.util.HashMap;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        var solutions = new Solution();
+        assert (11 == solutions.longestPalindrome("aaabbbccddee"));
+        assert (11 == solutions.longestPalindrome("aaabbccddee"));
+        assert (7 == solutions.longestPalindrome("xqaaaddee"));
+        assert (1 == solutions.longestPalindrome("abcde"));
+    }
+
+    static class Solution {
+        public int longestPalindrome(String s) {
+            var occurByChar = new HashMap<Integer, Integer>();
+            s.chars().forEach(c -> occurByChar.merge(c, 1, Integer::sum));
+
+            var occurSum = occurByChar.values().stream()
+                .mapToInt(n -> n % 2 == 0 ? n : n - 1)
+                .sum();
+
+            return occurSum < s.length() ? occurSum + 1 : occurSum;
+        }
+    }
+}
