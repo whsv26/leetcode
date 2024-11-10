8c2cf0908ebc16faeded9a6e8b102355af1e8bf6
whsv26 <whsv26@gmail.com>
2024-11-10 03:22:32 +0300

    Concatenation of Array java solution

---
+++ java-problems/src/main/java/org/whsv26/leetcode/string/ConcatenationOfArray.java
@@ -0,0 +1,34 @@
+package org.whsv26.leetcode.string;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+    public static void main(String[] args) {
+        var solutions = new Solution();
+        assert (Arrays.equals(
+            new int[]{1, 2, 1, 1, 2, 1},
+            solutions.getConcatenation(new int[]{1, 2, 1})
+        ));
+    }
+
+    static class Solution {
+        public int[] getConcatenation(int[] nums) {
+            var res = new int[nums.length * 2];
+            System.arraycopy(nums, 0, res, 0, nums.length);
+            System.arraycopy(nums, 0, res, nums.length, nums.length);
+            return res;
+        }
+    }
+
+    static class SlowSolution {
+        public int[] getConcatenation(int[] nums) {
+            var res = new int[nums.length * 2];
+
+            for (int i = 0; i < nums.length * 2; i++) {
+                res[i] = nums[i % nums.length];
+            }
+
+            return res;
+        }
+    }
+}
