1df471bfe0252c538504a02befffb486043ceda0
whsv26 <whsv26@gmail.com>
2024-11-10 03:05:19 +0300

    Score of a String java solution

---
+++ java-problems/src/main/java/org/whsv26/leetcode/string/ScoreOfAString.java
@@ -0,0 +1,20 @@
+package org.whsv26.leetcode.string;
+
+public class ScoreOfAString {
+    public static void main(String[] args) {
+        var solutions = new Solution();
+        assert (13 == solutions.scoreOfString("hello"));
+    }
+
+    static class Solution {
+        public int scoreOfString(String s) {
+            var sum = 0;
+
+            for (int i = 1; i < s.length(); i++) {
+                sum += Math.abs(s.charAt(i - 1) - s.charAt(i));
+            }
+
+            return sum;
+        }
+    }
+}
